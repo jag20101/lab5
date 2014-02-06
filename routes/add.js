607fee6360f21917716ef05c652aad3a5e110a2a
vagrant <vagrant@precise32.(none)>
2014-02-06 02:53:35 +0000

    bug fix

--- routes/add.js
+++ routes/add.js
@@ -5,6 +5,6 @@ exports.addFriend = function(req, res) {
 	res.render('add', data);
 	var newName = req.query.name;
 	var newDescription = req.query.description; 
-	var newFriendObj = {"name" newName, "description": newDescription, "imageURL": "http://lorempixel.com/400/400/people"};
+	var newFriendObj = {"name": newName, "description": newDescription, "imageURL": "http://lorempixel.com/400/400/people"};
 	data["friends"].push(newFriendObj);
 }
